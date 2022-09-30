35905c5cc3c5ce822facc579a4779f35df9848b3
Kamarajelango <rajelangovan18@gmail.com>
2022-09-30 14:44:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import './App.css';
+import Details from './Details';
+
+function App() {
+  let data=[
+    {
+      name:"kamaraj",
+      email:"kamaraj@gmail.com",
+      mobile:"9876543210"
+    },
+    {
+      name:"raj",
+      email:"raj@gmail.com",
+      mobile:"8976543210"
+    },
+    {
+      name:"karthick",
+      email:"karthick@gmail.com",
+      mobile:"743210"
+    }
+  ]
+  return( <>
+    <Details data={data}/>
+  </>
+  );
+}
+
+export default App;
---
+++ src/Details.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Details(props) {
+  return <>
+    {
+     props. data.map((e)=>{
+        return <div>
+          <div>{e.name}</div>
+          <div>{e.email}</div>
+          <div>{e.mobile}</div><br></br>
+        </div>
+      })
+    }
+  </>
+}
+
+export default Details
